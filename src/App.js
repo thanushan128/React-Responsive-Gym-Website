02901119ff6b33bcc966df6fbf5121644073035a
w3schools-test <test@w3schools.com>
2023-08-24 18:55:03 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+          <span style={{color: 'white'}}>This is React Responsive Gym Website</span>
+    </div>
+  );
+}
+
+export default App;
