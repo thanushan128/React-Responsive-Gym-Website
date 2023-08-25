349382e7037dc979fbeeee65a006d0d532150503
w3schools-test <test@w3schools.com>
2023-08-25 23:34:44 +0530

    Complete programs component

---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,32 @@
+// rafc
+import React from 'react'
+import './Programs.css'
+import {programsData} from '../../data/programsData'
+import RightArrow from '../../assets/rightArrow.png'
+
+export const Programs = () => {
+  return (
+    <div className="programs" id="programs">
+      {/* header */}
+      <div className="programs-header">
+        <span className='stroke-text'>Explore our</span>
+        <span>Programs</span>
+        <span className='stroke-text'>to share you</span>
+      </div>
+
+      {/* program category */}
+      <div className="programs-categories">
+        {programsData.map((program)=>(
+          <div className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now</span> <img src={RightArrow} alt="" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
