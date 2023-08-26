96a61bbd93feada2dbb37715af7d964b6df072d4
w3schools-test <test@w3schools.com>
2023-08-26 12:34:22 +0530

    Complete reasons component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import { Programs } from './components/Programs/Programs';
+import { Reasons } from './components/Reasons/Reasons';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
           <Hero/>
           <Programs/>
+          <Reasons/>
     </div>
   );
 }
