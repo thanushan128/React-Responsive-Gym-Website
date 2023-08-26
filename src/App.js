a4d1dce0b65df6f85a5729c85fae1f8bf3bc8b63
w3schools-test <test@w3schools.com>
2023-08-26 14:25:01 +0530

    Complete plans component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import { Programs } from './components/Programs/Programs';
+import Plans from './components/Plans/Plans';
 import { Reasons } from './components/Reasons/Reasons';
 
 
 function App() {
   return (
     <div className="App">
-          <Hero/>
-          <Programs/>
-          <Reasons/>
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
     </div>
   );
 }
